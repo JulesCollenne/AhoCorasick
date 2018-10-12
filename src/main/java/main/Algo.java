c901af3eeb339ea9a6f38c61b098ca8b7428176e
jules <jules.collenne@etu.univ-amu.fr>
2018-10-12 03:20:11 +0200

    Essai2

--- src/main/java/main/Algo.java
+++ src/main/java/main/Algo.java
@@ -3,8 +3,8 @@ package main;
 import java.util.*;
 
 public class Algo {
-    String[] mots_cle;
-    int[][] commande;
+    private String[] mots_cle;
+    private int[][] commande;
     int[] echec;
 
     public void tri_alphabet()
@@ -12,18 +12,71 @@ public class Algo {
         Arrays.sort(mots_cle);
     }
 
+    /**
+     * Fonction "Goto" qui créé l'automate à partir des mots clés
+     */
     public void creer_etats() {
-        int etat = 1;
+        int from = 0;
+        int to = 1;
+        int i = 0,j = 0;
 
-        //i = longueur mot, j = mot dans le tab mots_cle
-        int i = 0, j = 0;
+        commande[mots_cle[0].charAt(0)][0] = to;
+        to++;
 
-        commande[mots_cle[0].charAt(0)][0] = 0;
+        for(i=0;i<mots_cle[j].length();i++) {
+            for (j = 1; j < mots_cle.length; j++) {
+                if (mots_cle[j].charAt(i) != mots_cle[j - 1].charAt(i)) {
+                    commande[mots_cle[j].charAt(i)][from] = to;
+                    to++;
+                }
+            }
+            from++;
+        }
+    }
 
-        for (i = 1; i < mots_cle.length; i++) {
-            if (mots_cle[j].charAt(0) != mots_cle[i - 1].charAt(0)) {
-                commande[mots_cle[i].charAt(0)][0] = etat;
-                etat++;
+    /**
+     * La fonction "failure" qui renvoie l'etat dans lequel aller si jamais l'etat
+     * "etat" a fail
+     * @param etat, l'etat qui a fail
+     * @return le prochain etat
+     */
+    public int Fail(int etat){
+        return 0;
+    }
+
+    /**
+     * Renvoie la sortie de l'etat actuel
+     * @param etat actuel
+     * @return le mot clé trouvé ou null
+     */
+    public String Sortie(int etat){
+        return mots_cle[0];
+    }
+
+    /**
+     * Probablement a refaire avec une classe etat,
+     * pour l'instant, retourner -1 est equivalent a fail
+     * @param etat
+     * @param car
+     * @return
+     */
+    public int Goto(int etat, char car){
+        return -1;
+    }
+
+    public void AfficheSortie(int etat, int i){
+        System.out.println(i + " " + Sortie(etat));
+    }
+
+    public void Lecture(String texte){
+        int etat = 0;
+        int i;
+        for(i=0;i<texte.length();i++){
+            while(Goto(etat,texte.charAt(i)) == -1)
+                etat = echec[etat];
+            etat = Goto(etat,texte.charAt(i));
+            if(Sortie(etat) != null){
+                AfficheSortie(etat,i);
             }
         }
     }
