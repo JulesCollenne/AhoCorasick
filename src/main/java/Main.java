47354ff85929b69c436113b80686f5a1f37e8541
YannisNaidja <yannis.naidja@etu.univ-amu.fr>
2018-10-24 16:06:36 +0200

    Sans algo 4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.LinkedList;
 
@@ -5,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String[] entree = {"coup","arte","cle","table","cart"};
+        String[] entree = {"bonjour"};
         Arrays.sort(entree);
         Arborescence test = new Arborescence();
         test.CreerArbo(entree);
@@ -15,8 +17,12 @@ public class Main {
         fail.creer_fail();
         fail.afficher_fail();
 
-        String texte = utils.LisFichier("7germ10.txt");
-
+        String texte = null;
+        try {
+            texte = utils.readFile("7germ10.txt", StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         Lecture lect = new Lecture(test,fail);
         lect.Lire(texte);
     }
--- src/main/java/utils.java
+++ src/main/java/utils.java
@@ -1,29 +1,15 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class utils {
-    public static String LisFichier(String file){
-        File f = new File (file);
-        String texte = new String();
-        try {
-            FileReader fr = new FileReader(f);
-            try {
-                int c = fr.read();
-                while (c != -1) {
-                    texte += (char)c;
-                    c = fr.read();
-                }
-            }
-            catch (IOException exception) {
-                System.out.println("Erreur lors de la lecture : " + exception.getMessage());
-            }
-        }
-        catch(FileNotFoundException exception)
-        {
-            System.out.println("Le fichier n'a pas été trouvé");
-        }
-        return texte;
+
+    static String readFile(String path, Charset encoding)
+            throws IOException
+    {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
     }
+
 }
